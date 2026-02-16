2fb13975a3f006c4c76f0033ebf1aae9b9ed8361
LeoWhite555 <up230497@alumnos.upa.edu.mx>
2026-02-15 18:03:45 -0600

    hola

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,25 +1,40 @@
 module Helper exposing (..)
 
-import Html exposing (Html, div, h1, text, ul, li)
+import Html exposing (Html, div, h1, li, text, ul)
+
+
 
 -- 1. FUNCIONES MATEMÁTICAS
 -- add2: Recibe dos Int y los suma
+
+
 add2 : Int -> Int -> Int
-add2 a b = 
+add2 a b =
     a + b
 
+
+
 -- add3: Recibe tres Floats y los suma
+
+
 add3 : Float -> Float -> Float -> Float
-add3 x y z = 
+add3 x y z =
     x + y + z
 
+
+
 -- calc: Recibe dos números y una función (operación) para aplicarla
+
+
 calc : a -> a -> (a -> a -> b) -> b
-calc x y operation = 
+calc x y operation =
     operation x y
 
 
+
 -- 2. LISTAS Y RÉCORDS
+
+
 type alias Videogame =
     { title : String
     , releaseYear : Int
@@ -28,24 +43,47 @@ type alias Videogame =
     , genres : List String
     }
 
+
+
 -- languageNames: Extrae el campo .name de cada registro en la lista
+
+
 languageNames : List { record | name : String } -> List String
 languageNames list =
     List.map .name list
 
+
+
 -- onlyStudents: Si es Student devuelve el nombre, si no, un string vacío
+
+
 onlyStudents : List { record | name : String, uType : String } -> List String
 onlyStudents list =
-    List.map (\u -> if u.uType == "Student" then u.name else "") list
+    List.map
+        (\u ->
+            if u.uType == "Student" then
+                u.name
+
+            else
+                ""
+        )
+        list
+
+
 
 -- getVideogameGenres: Extrae la lista de géneros de cada videojuego
+
+
 getVideogameGenres : List Videogame -> List (List String)
 getVideogameGenres list =
     List.map .genres list
 
 
+
 -- 3. DATOS DE LA LAPTOP Y HTML
 -- El test espera que estos campos existan y sean Strings
+
+
 myLaptop =
     { ram = "16GB"
     , model = "XPS 13"
@@ -53,7 +91,11 @@ myLaptop =
     , screenSize = "13.4"
     }
 
+
+
 -- main: Genera el HTML con la estructura exacta que pide el test (div > div > ul > li)
+
+
 main : Html msg
 main =
     div []
